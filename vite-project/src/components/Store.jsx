129e2198c607c68dddabc466d3b814ed82bac5a5
panovaanastasiay <143810906+panovaanastasiay@users.noreply.github.com>
2024-04-14 10:20:32 -0700

    Add files via upload

---
+++ vite-project/src/components/Store.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import { products } from './Products'
+import IconSwitch from './IconSwitch'
+import ListView from './ListView';
+import CardsView from './CardsView'
+
+export default function Store() {
+  const [state, setState] = useState('grid');
+  
+  const handleLayoutSwitch = () => {
+    setState(state === 'grid' ? 'list' : 'grid');
+    console.log(state); 
+  };
+    
+  const icon = state === 'grid' ? 'view_list' : 'view_module';
+    
+  const displayView = state === 'grid' ? (
+    <CardsView cards={products} />
+  ) : (
+    <ListView items={products} />
+  );
+    
+    return (
+      <div>
+        <IconSwitch icon={icon} onSwitch={handleLayoutSwitch} />
+        {displayView}
+      </div>
+    );
+  };
